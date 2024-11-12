3fc7d1df275026f86c52c0580c4247ab3f7ebf52
juligeyash <julivalencia.alvarez@gmail.com>
2024-11-12 15:09:03 -0600

    Connected the api and updated the code to incorporate functions that make it work. When I look for a city it retrieves the correct temperature.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-function citySearch(event) {
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "07ee0e4da0c1ae2o4d5b3t0b38f6d86a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleCitySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let citySearchedElement = document.querySelector("#city");
-  citySearchedElement.innerHTML = searchInput.value;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let submitSearchElement = document.querySelector("#city-search");
-submitSearchElement.addEventListener("submit", citySearch);
+submitSearchElement.addEventListener("submit", handleCitySearch);
