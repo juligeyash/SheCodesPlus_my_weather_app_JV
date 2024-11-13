76c6a414ec4f9cdf83adaba37ac3e348e669ce50
juligeyash <julivalencia.alvarez@gmail.com>
2024-11-13 15:58:45 -0600

    Added JS for weather forecast and added CSS code to style the forecast display

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function refreshWeather(response) {
 function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
 
     return `${day} ${hours}:${minutes}`;
@@ -40,7 +40,37 @@ function handleCitySearch(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let submitSearchElement = document.querySelector("#city-search");
+submitSearchElement.addEventListener("submit", handleCitySearch);
+
 searchCity("Medellin");
+displayForecast();
+
 
 
 
