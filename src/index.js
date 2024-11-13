f9e2966ae72e1254d8ff94b3494ba6af54fbe35c
juligeyash <julivalencia.alvarez@gmail.com>
2024-11-13 17:32:28 -0600

    Updated JS code to display actual data from the forecast api, and added hyperlink to linked in profile

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ function refreshWeather(response) {
   windElement.innerHTML = `${response.data.wind.speed}Km/h`;
   timeElement.innerHTML = formatDate(date);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
+
+getForecast(response.data.city);
+
 }
 
 function formatDate(date) {
@@ -39,27 +42,49 @@ function handleCitySearch(event) {
   
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp *1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "07ee0e4da0c1ae2o4d5b3t0b38f6d86a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  //let days = ["Tue", "Wed", "Thu", "Fri", "Sat"] - this was used before the api was connected;
+
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+
     forecastHtml =
       forecastHtml +
       `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon">
+          <img src="${day.condition.icon_url}" /> </div>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}&#8451 / </strong>
           </div>
-          <div class="weather-forecast-temperature">9º</div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}&#8451</div>
         </div>
       </div>
     `;
+        }
   });
+    
 
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
@@ -69,8 +94,6 @@ let submitSearchElement = document.querySelector("#city-search");
 submitSearchElement.addEventListener("submit", handleCitySearch);
 
 searchCity("Medellin");
-displayForecast();
-
 
 
 
